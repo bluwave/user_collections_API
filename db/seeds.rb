a6d93b0d4f5f7a945602602c34b0edd9bc700d02
Garrett Richards <grichards@trulia.com>
2014-03-21 12:13:09 -0700

    more fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-john = User.create(:email => "Gr5@trulia.com", :apns_token => "dbe890d850ba483d4ea93f2cb6b8aa4deb00696d068917dbda68cecdd0e4ccc0")
-jane = User.create(:email => "Jane@doe.com", :apns_token => "996e85f2e7470f62a42d28800f126b1428b64450dda383f6a02d71a2cca8d84d")
+john = User.create(:email => "Gr5@trulia.com", :apns_token =>"996e85f2e7470f62a42d28800f126b1428b64450dda383f6a02d71a2cca8d84d" )
+jane = User.create(:email => "Jane@doe.com", :apns_token => "dbe890d850ba483d4ea93f2cb6b8aa4deb00696d068917dbda68cecdd0e4ccc0")
 c1 = Collection.create(:name => "homes my wife does not like" )
 CollectionConnection.create(:collection => c1, :user => john)
 CollectionConnection.create(:collection => c1, :user => jane)
