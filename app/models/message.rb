77ae5bca94f3ec1598b5c5e1fd43196e863bf280
Garrett Richards <grichards@trulia.com>
2014-03-24 13:59:55 -0700

    changed order

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,7 +6,7 @@ class Message < ActiveRecord::Base
 
     # Collection.joins(:users => :collections).where(:collection_connections => {:user_id => user_id})
     # Message.joins(:user => :collections).where(:collection_connections => {:user_id => user_id}).where(:pdp_id => pdp_id)
-    Message.where(:collection_id => Collection.joins(:users => :collections).where(:collection_connections => {:user_id => user_id}), :pdp_id => pdp_id)
+    Message.where(:collection_id => Collection.joins(:users => :collections).where(:collection_connections => {:user_id => user_id}), :pdp_id => pdp_id).order(:id)
 
   end
 end
